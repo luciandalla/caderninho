2c63c11141615d71ea521450fb4ebf52031206a2
Lucian Dalla <luciandalla.ld@gmail.com>
2023-03-28 17:44:12 -0300

    Criação de template para páginas HTML com menu responsivo utilizando bootstrap

--- views/gerais.py
+++ views/gerais.py
@@ -20,6 +20,7 @@ def home():
     if 'usuario' not in session or session['usuario'] == None:
         return redirect(url_for('login'))
 
+    usuario = session['usuario']
     valores_a_receber = total_a_receber()
     devedores = maiores_devedores()
     ultimos_lancamentos = Lancamentos.lancamentos_ultimos_30_dias()
@@ -27,5 +28,5 @@ def home():
 
     return render_template('home.html', valores_a_receber=valores_a_receber,
                            devedores=devedores, ultimos_lancamentos = ultimos_lancamentos,
-                           ultimos_pagamentos=ultimos_pagamentos)
+                           ultimos_pagamentos=ultimos_pagamentos, usuario=usuario)
 
